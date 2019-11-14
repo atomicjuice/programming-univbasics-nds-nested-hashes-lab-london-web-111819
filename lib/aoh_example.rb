ff9d79622d9b0154e4d3aa27e8730b2a43c3fda1
keemo <keemowatson1@gmail.com>
2019-11-14 17:27:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,10 +26,11 @@ end
 def aoh_lookup(aoh, row, key)
   aoh[row][key]
 end
-require"pry"
+# require"pry"
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   aoh[row][key] = new_value
-  binding.pry
+  aoh
+  # binding.pry
 end
